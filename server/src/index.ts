014c29be50c2e3559162942d065ebc5fd595e09d
Abhishek Gowda <axhigowda@Abhisheks-MacBook-Air.local>
2024-07-25 19:37:15 +0530

    intial commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 const mongoURI: string =
-  "mongodb+srv://machadop1407:UvOUTpZW7EH85MZl@personalfinancetracker.qb0edtk.mongodb.net/";
+  "mongodb+srv://abhishekgowdaofficial2611:aTvqTfSAoH5OtVFz@cluste01.s2poku3.mongodb.net/";
 
 mongoose
   .connect(mongoURI)
